609e4cd07af171ff58b3d698a124f8c0b90268ab
DavidGarea <gareadavid@gmail.com>
2020-07-10 21:52:16 +0200

    Numpy, Pandas

---
+++ week4/day1/delivery/my_visualizing_functions_module.py
@@ -0,0 +1,3 @@
+
+def hola():
+    return "hola"
---
+++ week4/day1/delivery/your_module.py
@@ -0,0 +1,11 @@
+# This file represents your module.
+# Write the code...
+import pandas as pd
+import numpy as np
+
+def mean_visualization():
+    """Draw the mean in a plot"""
+    return None
+
+x = mean_visualization()
+print(x)
